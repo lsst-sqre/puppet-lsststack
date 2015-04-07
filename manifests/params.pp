a73191e81d69fcfee68ca45aebbeed10ad1d4d55
Joshua Hoblitt <jhoblitt@cpan.org>
2015-04-07 16:41:58 -0700

    use java-1.8.0-openjdk on fedora 21

--- manifests/params.pp
+++ manifests/params.pp
@@ -38,7 +38,7 @@ class lsststack::params {
       ]
     }
     'RedHat': {
-      $dependency_packages = [
+      $base_packages = [
         'bison',
         'curl',
         'blas',
@@ -67,12 +67,21 @@ class lsststack::params {
         # needed for mysqlproxy
         'glib2-devel',
         # needed to build zookeeper
-        'java-1.7.0-openjdk',
+        #'java-1.7.0-openjdk',
         # needed to build git
         'gettext',
         'libcurl-devel',
         'perl-ExtUtils-MakeMaker',
       ]
+      case $::operatingsystem {
+        # fedora 21 moves to openjdk 1.8.0; el6 -> f20 have 1.7.0 available
+        'Fedora': {
+          $dependency_packages = concat($base_packages, 'java-1.8.0-openjdk')
+        }
+        default: {
+          $dependency_packages = concat($base_packages, 'java-1.7.0-openjdk')
+        }
+      }
     }
     default: { fail() }
   }
